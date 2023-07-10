786842abf334beb837562ca6047cc4d2c3dd4fd3
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2023-07-10 15:51:02 +0530

    updated threadlocal class

--- src/test/java/io/github/mfaisalkhatri/tests/ThreadLocalExample.java
+++ src/test/java/io/github/mfaisalkhatri/tests/ThreadLocalExample.java
@@ -13,26 +13,21 @@ public class ThreadLocalExample implements Runnable {
         this.threadLocal.set((int) (Math.random() * 40D));
         try {
             Thread.sleep(2000);
+
         } catch (final InterruptedException e) {
             throw new RuntimeException(e);
-        }
 
-        System.out.println(this.threadLocal.get());
+        }
         final long endTime = System.currentTimeMillis();
         final NumberFormat formatter = new DecimalFormat("#0.00000");
-        System.out.print("Execution time is " + formatter.format((endTime - startTime) / 1000d) + " seconds");
+        System.out.println("Thread Id - " + this.threadLocal.get() + " Execution time is " + formatter.format((endTime - startTime) / 1000d) + " seconds" + "\n");
     }
 
     public static void main(final String[] args) {
         final ThreadLocalExample threadLocalExample = new ThreadLocalExample();
         final Thread t1 = new Thread(threadLocalExample);
         final Thread t2 = new Thread(threadLocalExample);
-
-        //start time and end time needs to be printed in he console
         t1.start();
-
         t2.start();
-
     }
-
 }
